8384d96de1b7066ba055656eaa127a56602903c8
Gabriel Creatto <gabriel.creatto@portoseguro.com.br>
2023-01-02 15:39:48 -0300

    created service users

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response, Express } from 'express';
+import express, { Express } from 'express';
 import bodyParser from 'body-parser';
-import { connect_database } from './models/model';
+import { connect_database } from '../models/model';
+import user from '../services/userService';
 
 export async function init_routers(): Promise<Express> {
 
-    const app = express();
+    const app: Express = express();
     app.use(bodyParser.json());
-    app.use('/') //someapp)
+    app.use('/', user) //someapp)
 
     await connect_database();
 
---
+++ src/services/userService.ts
@@ -0,0 +1,5 @@
+import express, { Request, Response, Express } from "express";
+
+const router = express.Router();
+
+export default router
