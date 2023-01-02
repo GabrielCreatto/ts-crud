ec8ff94b12c2146f69e44387f2b8490454c6ab14
Gabriel Creatto <gabriel.creatto@portoseguro.com.br>
2023-01-02 18:36:25 -0300

    add delete user

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -49,4 +49,25 @@ router.get('/', async (req: Request, res: Response) => {
     }
 });
 
+router.delete('/:id', async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+
+        const query = { _id: new ObjectId(id) };
+        const result = await collections.users?.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Successfully removed user with id ${id}`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove user with id ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`User with id ${id} does not exist`);
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
+});
+
 export default router
